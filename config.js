dea80e0325810e74a7bd13f09bae7aea7a7c965c
deeno201 <86836907+deeno201@users.noreply.github.com>
2021-07-09 09:44:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     introTitle: 'Xin chào em pé của Dịu Ngox ᵔ ᴥ ᵔ',
     introDesc: `Em có điều này muốn hỏi anh nhớ phải trả lời thật lòng nhaa `,
     btnIntro: 'Game là dễ luônnn',
-    title: 'Liệu rằng có em bên đời thì Quốc Toàn có vui hem ._.',
+    title: 'Liệu rằng Quốc Toàn có yêu em nhất hem ._.',
     desc: 'Quân tử nhất ngôn không chơi trò cánh bướm dối gian nhó!!!',
     btnYes: 'Yêu ơi là yêu luôn yêu vl ',
     btnNo: 'Em mơ à :P Ai thèmmm',
